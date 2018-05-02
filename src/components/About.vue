5c76c3cb4c56650ed549bcf25477c35ee0e177c9
Rodrigo Ferreira <rodrigofepy@gmail.com>
2018-05-02 19:07:33 -0400

    Passing parameters through the URL

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hello {{their_name}} This is an about page</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quaerat? Dolorem expedita illo neque quasi aspernatur tenetur suscipit, quod excepturi eaque assumenda, cum eius corporis unde delectus? Illum, distinctio ad.</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
