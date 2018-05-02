271040a5c26614f9803814e8c7faec0e0ca34a02
Rodrigo Ferreira <rodrigofepy@gmail.com>
2018-05-02 18:27:03 -0400

    Adding styles to the app

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,23 @@ export default {
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
   body {
-    background-color: #eeeeee;
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', sans-serif;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    align-items: center;
+  }
+
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
+  
+  #app {
+      width: 50%;
   }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,7 +5,7 @@
         <li v-for="(data,index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
       </ul>
 
-      <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
+      <p>These are the skills that you posses.</p>
     </div>
   </div>
 </template>
@@ -29,10 +29,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .alert {
-    width: 100%;
-    height: 30px;
+  .holder {
+    background: #fff;
+  }
 
-    background-color: yellow;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
   }
 </style>
