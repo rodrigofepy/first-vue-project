f85bd2118913cf4f6e813f1b263ca78ef85cbe1e
Rodrigo Ferreira <rodrigofepy@gmail.com>
2018-05-02 19:04:01 -0400

    Add routing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <Skills />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
@@ -24,15 +29,29 @@ export default {
     display: grid;
     grid-template-rows: auto;
     justify-items: center;
-    align-items: center;
+    padding-top: 50px;
   }
 
   body, html {
     margin: 0;
     height: 100%;
   }
-  
+
   #app {
       width: 50%;
+  }
+
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
   }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quaerat? Dolorem expedita illo neque quasi aspernatur tenetur suscipit, quod excepturi eaque assumenda, cum eius corporis unde delectus? Illum, distinctio ad.</p>
+  </div>
+</template>
