8165d40f95237cee86608e7f8c737bd314f11337
Rodrigo Ferreira <rodrigofepy@gmail.com>
2018-05-02 19:13:19 -0400

    Add the remove method

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -12,7 +12,10 @@
 
       <ul>
         <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
-          <li v-for="(data,index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+          <li v-for="(data,index) in skills" :key='index'>
+            {{ data.skill }}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
         </transition-group>
       </ul>
 
@@ -46,7 +49,9 @@ export default {
           console.log('invalid')
         }
       })
-      
+    },
+    remove(id) {
+      this.skills.splice(id, 1)
     }
   }
 }
@@ -55,6 +60,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
   .holder {
     background: #fff;
@@ -120,5 +126,10 @@ export default {
     100% {
       transform: scale(1);
     }
+  }
+
+  i {
+    float: right;
+    cursor: pointer;
   }
 </style>
